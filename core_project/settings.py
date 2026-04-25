5e4d7784c484766139ff892fbb938c3c48265180
Dimas SP <satriapasha269@gmail.com>
2026-04-25 19:09:50 +0700

    update

--- core_project/settings.py
+++ core_project/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n(e%0^0g)onj+6ur)dfe2%fy73fd8$2ezfj*=l(*-sa6051kb='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# Masukin domain lo dan domain render
+ALLOWED_HOSTS = ['dimas-sp.me', 'www.dimas-sp.me', '.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Ini folder tempat lo nyimpen CSS/Gambar waktu ngoding
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# INI YANG BARU: Folder tempat Render bakal ngumpulin semua gambar/CSS lo
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'porto/static'),
 ]
